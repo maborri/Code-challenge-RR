be645bb6a423f0cae1a4b48ba6ecf3220ed63add
maborri <micaela.a.ledesma.96@gmail.com>
2019-12-23 17:53:43 -0300

    Updates

---
+++ precache-manifest.cd3be31f956c9964fe60ee6fccb92ec0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887e443b4d499738369e3d9955155b0f",
+    "url": "/RR/index.html"
+  },
+  {
+    "revision": "4968e2ff0e8f64593f66",
+    "url": "/RR/static/css/main.85e3e435.chunk.css"
+  },
+  {
+    "revision": "17d8f9fb23968d1fc03b",
+    "url": "/RR/static/js/2.6dade064.chunk.js"
+  },
+  {
+    "revision": "5a0d989568a936270ef6c2e0e2d0229c",
+    "url": "/RR/static/js/2.6dade064.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4968e2ff0e8f64593f66",
+    "url": "/RR/static/js/main.900bf914.chunk.js"
+  },
+  {
+    "revision": "af00ba5a872e2f598235",
+    "url": "/RR/static/js/runtime-main.2c269195.js"
+  },
+  {
+    "revision": "151a7781205562b29bfa98d54ae4a101",
+    "url": "/RR/static/media/Z-packaging.151a7781.png"
+  },
+  {
+    "revision": "a96c7489bfd483ea8cf9c28d36fee7b9",
+    "url": "/RR/static/media/flower-left.a96c7489.png"
+  },
+  {
+    "revision": "f736f777f70458e0554dcb29ef715612",
+    "url": "/RR/static/media/flower-right.f736f777.png"
+  },
+  {
+    "revision": "272c8e9358187f658a4cc7ab9197fb11",
+    "url": "/RR/static/media/hexagon.272c8e93.png"
+  }
+]);
